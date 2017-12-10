de6f8c47ebff3029e2e9c242fe7ebc7dd7184abe
sdduffield <samuel.duffield@stlcop.edu>
2017-12-09 19:38:13 -0600

    final assignment submission

--- intro to r.R
+++ intro to r.R
@@ -115,3 +115,72 @@ site2_npp_day <- site2_g_carbon_m2_day * site2_area_m2
 total_npp_day <- site1_npp_day + site2_npp_day
 total_npp_year <- total_npp_day * 365
 print(total_npp_year)
+
+
+#Exercise 6
+
+#import and plot monthly mean precipitation data from working directory
+
+ppt_data <- read.csv("./data/gainesville-precip.csv", header = FALSE)
+monthly_mean_ppt <- colMeans(ppt_data)
+plot(monthly_mean_ppt, type = "l", xlab = "Month", ylab = "Mean Precipitation")
+
+
+#Exercise 7
+number_of_birds <- c(28, 32, 1, 0, 10, 22, 30, 19, 145, 27, 
+                     36, 25, 9, 38, 21, 12, 122, 87, 36, 3, 0, 5, 55, 62, 98, 32, 
+                     900, 33, 14, 39, 56, 81, 29, 38, 1, 0, 143, 37, 98, 77, 92, 
+                     83, 34, 98, 40, 45, 51, 17, 22, 37, 48, 38, 91, 73, 54, 46,
+                     102, 273, 600, 10, 11)
+
+#7.1
+length(number_of_birds)
+
+#7.2
+number_of_birds[42]
+
+#7.3
+number_of_birds[length(number_of_birds)]
+
+#7.4
+sum(number_of_birds)
+
+#7.5
+min(number_of_birds)
+
+#7.6
+max(number_of_birds)
+
+#7.7
+mean(number_of_birds)
+
+
+#Exercise 8
+
+length <- c(2.2, 2.1, 2.7, 3.0, 3.1, 2.5, 1.9, 1.1, 3.5, 2.9)
+width <- c(1.3, 2.2, 1.5, 4.5, 3.1, 2.8, 1.8, 0.5, 2.0, 2.7)
+height <- c(9.6, 7.6, 2.2, 1.5, 4.0, 3.0, 4.5, 2.3, 7.5, 3.2)
+
+#8.1
+volume <- length * width * height
+print(volume)
+
+#8.2
+sum(volume)
+
+#8.3
+height[length > 2.5]
+
+#Exercise 9 
+shrub_volume <- read.csv("./data/shrub-dimensions-labeled.csv")
+
+#9.1
+shrub_volume$length
+
+#9.2
+shrub_volume$length * shrub_volume$width * shrub_volume$height
+
+
+
+
+
